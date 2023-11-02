5bda35ab1b86ce3f8e35d8ade60e8d22c5cc8a9c
JuanDVejarano <juanjdv_200@hotmail.com>
2023-11-01 19:03:41 -0500

    Condicionales

---
+++ src/condicionales.py
@@ -0,0 +1,17 @@
+
+# Ejemplo de condicional if-else
+numero = 10
+if numero > 0:
+    print("El número es positivo")
+else:
+    print("El número es negativo o cero")
+
+# Ejemplo de condicional anidado if-else
+edad = 18
+if edad >= 18:
+    if edad == 18:
+        print("Recién cumpliste 18 años")
+    else:
+        print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
